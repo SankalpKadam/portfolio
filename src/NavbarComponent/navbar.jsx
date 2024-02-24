e587b3e488e794ec9b8eb5f9b77da1db58d2fd0f
SankalpKadam <sankalptp07@gmail.com>
2024-02-24 15:58:01 -0600

    tailwind setup complete

---
+++ src/NavbarComponent/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='navbar flex bg-white'>
+      <div className='navbar__logo lg:text-2xl text-white'>
+        Sankalp Kadam
+      </div>
+      <div className='navbar__options'>
+        Download Resume
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;

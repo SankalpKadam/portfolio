1d90c9a78b1133b319f6abf3ed735f22470e8566
SankalpKadam <sankalptp07@gmail.com>
2024-02-26 15:25:28 -0600

    integrated star background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navbar from './NavbarComponent/navbar';
-
+import Homepage from './HomePage/Homepage';
+import StarsCanvas from './StarsBackground/StarBackground';
 function App() {
   return (
     <div className="App">
+      <StarsCanvas/>
       <Navbar/>
+      <Homepage/>
     </div>
   );
 }
---
+++ src/HomePage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Homepage.css'
+import StarsCanvas from '../StarsBackground/StarBackground';;
+const Homepage = () => {
+  return (
+    <div className='homepage'>
+      {/* <StarsCanvas/> */}
+      <p className='text-3xl white text-center bg-black'>Hello</p>
+    </div>
+  )
+}
+
+export default Homepage

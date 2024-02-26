da9536bf51827c8cb10046d15d09fd4ee3c4569a
SankalpKadam <sankalptp07@gmail.com>
2024-02-25 21:47:17 -0600

    navbar functionality done

--- src/NavbarComponent/navbar.jsx
+++ src/NavbarComponent/navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-
+import './navbar.css'
 const Navbar = () => {
   return (
-    <div className='navbar flex bg-white'>
-      <div className='navbar__logo lg:text-2xl text-white'>
-        Sankalp Kadam
+    <div className='navbar'>
+      <div className='navbar__logo'>
+        <p className='navbar__firstName lg:text-4xl md:text-2xl font-bold'>Sankalp</p>
+        <p className='navbar__lastName'>Kadam</p>
       </div>
-      <div className='navbar__options'>
-        Download Resume
+      <div className='navbar__options flex'>
+        <div className='navbar__downloadBtn'>
+          Download Resume
+        </div>
       </div>
     </div>
   )

219849aa98cf2ae3a2d84a6828a723d98154fca4
SankalpKadam <sankalptp07@gmail.com>
2024-02-27 11:19:42 -0600

    created the about me page

---
+++ src/About/AboutMe.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './AboutMe.css'
+const AboutMe = () => {
+  return (
+    <div className='aboutme'>
+      <p className='text-4xl text-transparent font-semibold'>ABOUT ME</p>
+    </div>
+  )
+}
+
+export default AboutMe

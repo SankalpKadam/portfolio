4ebb87b3af34b651ba008dadcda0fa85fd6288b0
SankalpKadam <sankalptp07@gmail.com>
2024-02-27 11:19:08 -0600

    worked on responsiveness

--- src/HomePage/Homepage.jsx
+++ src/HomePage/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Homepage.css'
 import StarsCanvas from '../StarsBackground/StarBackground';import Banner from '../Banner/Banner';
+import AboutMe from '../About/AboutMe';
 ;
 const Homepage = () => {
   return (
     <div className='homepage'>
         <Banner/>
+        <AboutMe/>
       {/* <StarsCanvas/> */}
       {/* <p className='text-3xl white text-center bg-white'>Hello</p> */}
     </div>

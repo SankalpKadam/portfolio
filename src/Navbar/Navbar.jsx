2463a75b4fdf408e3895d7410f47763c3acbc484
SankalpKadam <sankalptp07@gmail.com>
2024-03-15 01:15:14 -0500

    Completed responsive navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -2,15 +2,49 @@ import React from 'react'
 import './Navbar.css'
 const Navbar = () => {
   return (
-    <div className='navbar lg:py-8 lg:px-64 px-4 py-4'>
-      <div className='navbar__logo'>
-        <p className='lg:text-3xl 
-        text-2xl 
+    <div className='navbar 
+    lg:py-8 
+    lg:px-32
+    md:px-16 
+    px-4 
+    py-4 
+    flex 
+    flex-row 
+    text-white 
+    justify-between
+    items-center
+    backdrop-blur-sm
+    z-50'>
+      <div className='navbar__logo
+      flex
+      flex-row
+      items-center'>
+        <img src={require("../sk-high-resolution-logo-white-transparent.png")} alt="" className='h-10
+        pr-3
+        '/>
+        <div>
+        <p className='lg:text-3xl
+        md:text-2xl
+        text-xl 
         font-bold 
         text-white'>Sankalp</p>
         <p className='lg:text-xl
-        text-lg
-        text-white'>Kadam</p>
+        md:text-lg
+        text-md
+        text-white
+        border-x-gray-400
+        -mt-2'>Kadam</p>
+        </div>
+      </div>
+      <div className='border-2
+      border-navbarColor
+      rounded-xl
+      px-4
+      py-2
+      lg:text-xl
+      text-sm
+      '>
+        Download Resume
       </div>
     </div>
   )

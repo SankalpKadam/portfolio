f327fa126e3b638a6e88d6ba3e8dc9741b8b5cb3
SankalpKadam <sankalptp07@gmail.com>
2024-03-15 01:15:53 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'navbarColor':"#827D7D"
+      }
+    },
+    
   },
   plugins: [],
 }

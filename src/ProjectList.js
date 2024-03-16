641025dcd95d0c053a1cf3c15a109d4f13f86aa0
SankalpKadam <sankalptp07@gmail.com>
2024-03-16 17:25:22 -0500

    Added images and created a list of projects
    This list will be used to render the individual projects wherever necessary.

---
+++ src/ProjectList.js
@@ -0,0 +1,47 @@
+export const projectList =[
+    {
+        id:1,
+        Title:"NotFlix",
+        ShortText:"A full stack project with major focus on frontend and user authentication using OAuth which emulates the design and feel of the original netflix web app. Intergrated 3rd party API to fetch movie data and used MongoDB to store user data, complemented with Google Authentication using Firebase",
+        Skills:["TypeScript","Next.Js","Firebase","MongoDB","Tailwind CSS"],
+        img:"/notflix.png",
+        Github:'https://github.com/SankalpKadam/notflix',
+        Live:"https://notflix-swart.vercel.app/"
+    },
+    {
+        id:2,
+        Title:"Blogee",
+        ShortText:"A college blog website where college students can post blogs regarding the various events that are happening in the campus, and also use media to share event experience. A full stack web application which uses MongoDB to handle data and utilizes a REST API architecture to handle requests.",
+        Skills:["JavaScript","HTML","CSS","MongoDB","Express","Node.Js"],
+        img:'/blogee.png',
+        Github:'https://github.com/SankalpKadam/Blog_Website',
+        Live:null
+    },
+    {
+        id:3,
+        Title:"Concord",
+        ShortText:"Put simply, this project replicates the functionality of Discord, incorporating various features found in the original application. The user interface is constructed using React, Material UI, HTML, CSS, and JavaScript. For the backend, Firebase is employed, specifically utilizing Firestore for real-time database functionality and Authentication through OAuth.",
+        Skills:["JavaScript", "React.Js","Redux","Firebase","Firebase Auth"],
+        img:"/concord.png",
+        Github:"https://github.com/SankalpKadam/ConCord",
+        Live:null
+    },
+    {
+        id:4,
+        Title:"Volunteer Research Hub",
+        ShortText:"A portal made for students who want to do volunteer research under supervision of professors. Helps to maintain a centralized portal for tracking user activity, including but not limited to task management, report submission and even generating recommendations.",
+        Skills:["JavaScript","React.Js","PHP","Laravel","MySQL","Figma"],
+        img:'/volunteer.png',
+        Github:"https://github.com/SankalpKadam/volunteer_program",
+        Live:"https://ssk2320.uta.cloud/"
+    },
+    {
+        id:5,
+        Title:"Keep Notes",
+        ShortText:"A reponsive web application which helps users to keep track of their notes and emulates the behavior of the Google Keep.",
+        Skills:["JavaScript","React.Js","MongoDB","Node.Js","Express"],
+        img:"/keep.png",
+        Github:"https://github.com/SankalpKadam/Google_Keep_Clone",
+        Live:null
+    }
+]

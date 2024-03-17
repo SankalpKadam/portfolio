879f5a789a84c50fc213f8d650a09d66db9d5cc2
SankalpKadam <sankalptp07@gmail.com>
2024-03-17 01:41:19 -0500

    Initial react router setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
+
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './Homepage/Homepage';
 import Navbar from './Navbar/Navbar';
 import StarsCanvas from './StarsBackground';
-
 function App() {
   return (
-    <div className="App h-full bg-[url('./LooperGroup2.png')] bg-no-repeat">
-      <StarsCanvas/>
-      <Navbar/>
-      <Homepage/>
-    </div>
+    <Routes>
+
+      <Route path='/' element={<Homepage/>}/>
+    </Routes>
   );
 }
 

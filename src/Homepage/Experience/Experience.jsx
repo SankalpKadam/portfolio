c7b69b917489d920d442c44f2e80d809e6342f93
SankalpKadam <sankalptp07@gmail.com>
2024-03-16 23:44:39 -0500

    Update Experience.jsx

--- src/Homepage/Experience/Experience.jsx
+++ src/Homepage/Experience/Experience.jsx
@@ -3,9 +3,14 @@ import React from 'react'
 const Experience = () => {
     const workdone = [`Enhanced the efficiency of the backend API by integrating PHP Laravel and MySQL and performed comprehensive testing comprising of 200 requests for validation of the API’s functionality using Postman.`,
         `Collaborated with senior engineers to enhance the company’s website, resulting in a 15% increase in user engagement through the development of a user-friendly and visually appealing user interface using Nuxt.Js.`,
+        `Designed and implemented a robust REST API capable of efficiently handling 100 concurrent requests.`,
         `Developed an MVC model in Laravel to optimize code structure which helped to simplify the testing process by 10%.`,
         `Utilized AWS Lambda and harnessed its Lex chatbot to develop a robust voice communication system for a robotics project, achieving an impressive 95% accuracy in recognizing spoken language and generating responses in three different languages.`,
-        `Designed and implemented a robust REST API capable of efficiently handling 100 concurrent requests.`
+        `Contributed to develop a path planning algorithm for a robotics project with over 92% accuracy and used Python to gather data from multiple sensors and generate a map from the current location to the desired destination`,
+        `Contributed to developing a live emotion and hand movement detection system and designed robot responses accordingly.`,
+        `Developed a simple image search functionality for a client product with 97% accuracy and used Django server accompanied with a Convolutional Neural Network to process the given image and generate recommendations based on the analysis.`,
+        `Collaborated with senior engineers to work with Docker containerization and followed agile methodology using ClickUp to manage all tasks and communications.`
+        
     ]
     const usedSkills = ["Python", "JavaScript", "AWS","MySQL","PHP","Laravel","Docker"]
     return (
@@ -14,7 +19,8 @@ const Experience = () => {
             flex 
             flex-col 
             items-center
-            mt-[150px]'>
+            mt-[150px]
+            '>
             <div className='text-transparent 
                 lg:text-7xl 
                 md:text-6xl 
@@ -37,10 +43,12 @@ const Experience = () => {
                 sm:px-0
                 px-2
                 mt-10
+                z-50
             '>
                 <div className='flex
                 flex-col
                 flex-1
+                z-50
                     '>
                     <p className='
                     gradient__text
@@ -95,6 +103,7 @@ const Experience = () => {
             text-white
             text-xs
             md:text-lg
+            z-50
             '>
                 <ul className='
                 list-disc'>
